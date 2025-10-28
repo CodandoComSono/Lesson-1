db52ce45ef621f0e28797145271314e29e401183
ThiagoAGJ <thaquino.dev@gmail.com>
2025-10-28 13:56:59 -0300

    Update CurrencyConverter.py

--- CurrencyConverter.py
+++ CurrencyConverter.py
@@ -1,14 +1,14 @@
-real = float(input('Quanto reais você tem na carteira? R$: '))
-dolar = 5.38
-euro = 6.27
-pesochileno = 0.0057
+real = float(input('How many reais do you have in your wallet? R$: '))
+usd_rate = 5.38
+eur_rate = 6.27
+clp_rate = 0.0057
 
-condolar = real / dolar
-coneuro = real / euro
-conchileno = real / pesochileno
+usd = real / usd_rate
+eur = real / eur_rate
+clp = real / clp_rate
 
 print('=' * 80)
-print(f'Com R${real:.2f} você consegue comprar US${condolar:.2f} dólares hoje.')
-print(f'Com R${real:.2f} você consegue comprar EUR€{coneuro:.2f} euros hoje.')
-print(f'Com R${real:.2f} você consegue comprar CLP${conchileno:,.0f} pesos chilenos hoje.')
+print(f'With R${real:.2f}, you can buy US${usd:.2f} today.')
+print(f'With R${real:.2f}, you can buy €{eur:.2f} today.')
+print(f'With R${real:.2f}, you can buy CLP${clp:,.0f} today.')
 print('=' * 80)
