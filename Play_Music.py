99ae68d34b27a38370d0a374e1a914f246a253e1
ThiagoAGJ <thaquino.dev@gmail.com>
2025-10-29 21:39:43 -0300

    Delete Play_Music.py

--- Play_Music.py
+++
@@ -1,7 +0,0 @@
-import pygame
-pygame.init()
-pygame.mixer.init()
-pygame.mixer.music.load(r'C:\Users\thiago.agjunior\Downloads\Python\Kenny-G.mp3')
-pygame.mixer.music.play()
-while pygame.mixer.music.get_busy():
-    pass
